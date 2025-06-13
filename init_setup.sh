b69dbc22d8eaeec3a3a280ea0d4ecba4e79da858
Vishal2387 <vishalselvakumar23@gmail.com>
2025-06-13 17:33:27 +0530

    added annotations for init_setup.sh

--- init_setup.sh
+++ init_setup.sh
@@ -2,7 +2,7 @@ echo [$(date)]: "START"
 echo [$(date)]: "creating env with python 3.8 version" 
 conda create --prefix ./env python=3.8 -y
 echo [$(date)]: "activating the environment" 
-source activate ./env
+source activate ./env     #since we are creating env in source sh file we need to activate conda env with this command - source activate <env name>
 echo [$(date)]: "installing the dev requirements" 
 pip install -r requirements_dev.txt
 echo [$(date)]: "END" 
